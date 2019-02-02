9f78cbc8457b930c58492020c7f47bc41b85a4f4
Takao Mizuno <takao.mizuno110@gmail.com>
2019-02-03 02:43:30 +0900

    initial init

---
+++ train.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+import argparse
+import os
+import sys
+
+import torch
+
+
+if __name__ == '__main__':
+    pass
---
+++ utils.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+import argparse
+import os
+import sys
